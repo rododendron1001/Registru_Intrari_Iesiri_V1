f6afe88abd6312ee10892350d31e64ceab185495
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-08-22 20:13:31 +0300

    Add Mainwe Models

---
+++ Registru_Intrari_Iesiri/Registru_Intrari_Iesiri/Models/EntryStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registru_Intrari_Iesiri.Models
+{
+    public enum EntryStatus
+    {
+        Registered =0,
+        InProgress =10,
+        Resolved = 20
+    }
+}
---
+++ Registru_Intrari_Iesiri/Registru_Intrari_Iesiri/Models/InOutRegisterEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registru_Intrari_Iesiri.Models
+{
+    public class InOutRegisterEntry
+    {
+        public int Id { get; set; }
+        public int DocNumber { get; set; }
+        public DateTime DocDate { get; set; }
+        public Customer Customer { get; set; }
+        public string Description { get; set; }
+        public EntryStatus Status { get; set; }
+        public string Resolution { get; set; }
+    }
+}
