dcef5049ff5fb29b828f910ee446d2a84358ea19
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-08-22 19:42:24 +0300

    Initial Project Setup

---
+++ Registru_Intrari_Iesiri/Registru_Intrari_Iesiri/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Registru_Intrari_Iesiri.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
